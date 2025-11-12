0c829416db759a7ebb39dab1e01f6712cbd79bbe
Alberto Cuesta Marcos <alber91@gmail.com>
2025-11-12 12:08:00 +0100

    Minor updates

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1301,7 +1301,7 @@
  * Override with M92 (when enabled below)
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 400, 400, 400, 98 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 400, 400, 400, 97 }
 
 /**
  * Enable support for M92. Disable to save at least ~530 bytes of flash.
@@ -1313,7 +1313,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 35, 35, 7, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 50, 35, 14, 7 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -1326,7 +1326,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 100, 50, 25, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 80, 40, 20, 10000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -1341,9 +1341,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          50    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_ACCELERATION          40    // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   50    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_TRAVEL_ACCELERATION   40    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk limits (mm/s)
@@ -1353,7 +1353,7 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define CLASSIC_JERK
+//#define CLASSIC_JERK
 #if ENABLED(CLASSIC_JERK)
   #define DEFAULT_XJERK 2.0
   #define DEFAULT_YJERK 2.0
@@ -2381,7 +2381,7 @@
 #endif
 
 // Homing speeds (linear=mm/min, rotational=°/min)
-#define HOMING_FEEDRATE_MM_M { (45*60), (35*60), (6*60) }
+#define HOMING_FEEDRATE_MM_M { (35*60), (25*60), (12*60) }
 
 // Edit homing feedrates with M210 and MarlinUI menu items
 //#define EDITABLE_HOMING_FEEDRATE
--- README.md
+++ README.md
@@ -9,6 +9,10 @@ Custom [Marlin](https://github.com/MarlinFirmware/Marlin) firmware for [Krotos3d
 M777 - HW bed leveling
 ```
 New custom gcode to use the 4-motor Z axis for bed leveling
+By default will run 5 times until the bed is leveled. Can be modified with R parameter
+```
+M777 R1
+```
 
 ## Remarkable commands
 ```
@@ -31,7 +35,7 @@ M851 Z-X.XX - sets nozzle diff
 
 ## Configure nozzle height
 ```
-M777 - HW bed leveling
+M777 R1 - HW bed leveling
 G29 - Bed leveling
 G28 Z - Home z at center
 G91 - Relative positioning
@@ -54,8 +58,8 @@ M851 Z-X.XX - Set the nozzle height to X.XX
    - Updated e driver to drv8825
    - Z_MIN_ENDSTOP_INVERTING to true
    - Z_MIN_PROBE_ENDSTOP_INVERTING to true
-   - DEFAULT_AXIS_STEPS_PER_UNIT to { 400, 400, 400, 98 }
-   - DEFAULT_MAX_ACCELERATION to { 100, 50, 25, 10000 }
+   - DEFAULT_AXIS_STEPS_PER_UNIT to { 400, 400, 400, 97 }
+   - DEFAULT_MAX_ACCELERATION
    - Update ACCELERATION
    - define USE_PROBE_FOR_Z_HOMING
    - define FIX_MOUNTED_PROBE
